742fcda001160e8127ac15a7462c28ae831a0c02
Belinda Sergent <91791901+adnileb19@users.noreply.github.com>
2022-10-06 18:13:09 +0100

    add boilerplate code to the discount() method in the basket

--- src/main/java/kata/supermarket/Basket.java
+++ src/main/java/kata/supermarket/Basket.java
@@ -7,51 +7,49 @@ import java.util.Collections;
 import java.util.List;
 
 public class Basket {
-    private final List<Item> items;
+	private final List<Item> items;
 
-    public Basket() {
-        this.items = new ArrayList<>();
-    }
+	public Basket() {
+		this.items = new ArrayList<>();
+	}
 
-    public void add(final Item item) {
-        this.items.add(item);
-    }
+	public void add(final Item item) {
+		this.items.add(item);
+	}
 
-    List<Item> items() {
-        return Collections.unmodifiableList(items);
-    }
+	List<Item> items() {
+		return Collections.unmodifiableList(items);
+	}
 
-    public BigDecimal total() {
-        return new TotalCalculator().calculate();
-    }
+	public BigDecimal total() {
+		return new TotalCalculator().calculate();
+	}
 
-    private class TotalCalculator {
-        private final List<Item> items;
+	private class TotalCalculator {
+		private final List<Item> items;
 
-        TotalCalculator() {
-            this.items = items();
-        }
+		TotalCalculator() {
+			this.items = items();
+		}
 
-        private BigDecimal subtotal() {
-            return items.stream().map(Item::price)
-                    .reduce(BigDecimal::add)
-                    .orElse(BigDecimal.ZERO)
-                    .setScale(2, RoundingMode.HALF_UP);
-        }
+		private BigDecimal subtotal() {
+			return items.stream().map(Item::price).reduce(BigDecimal::add).orElse(BigDecimal.ZERO).setScale(2,
+					RoundingMode.HALF_UP);
+		}
 
-        /**
-         * TODO: This could be a good place to apply the results of
-         *  the discount calculations.
-         *  It is not likely to be the best place to do those calculations.
-         *  Think about how Basket could interact with something
-         *  which provides that functionality.
-         */
-        private BigDecimal discounts() {
-            return BigDecimal.ZERO;
-        }
+		/**
+		 * TODO: This could be a good place to apply the results of the discount
+		 * calculations. It is not likely to be the best place to do those calculations.
+		 * Think about how Basket could interact with something which provides that
+		 * functionality.
+		 */
+		private BigDecimal discounts() {
+			DiscountScheme byOneGetOneFreeDiscount = new ByOneGetOneFree();
+			return byOneGetOneFreeDiscount.discountPrice();
+		}
 
-        private BigDecimal calculate() {
-            return subtotal().subtract(discounts());
-        }
-    }
+		private BigDecimal calculate() {
+			return subtotal().subtract(discounts());
+		}
+	}
 }
